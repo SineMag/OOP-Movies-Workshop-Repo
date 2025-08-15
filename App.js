d739e229b9d75e41f4edfc3cf0a7a7eac41b9494
SineMag <snenhlanhlamagubane99@gmail.com>
2025-08-15 12:32:22 +0200

    do-while loop practice.

--- App.js
+++ App.js
@@ -1,38 +1,66 @@
-// let sentence = "Hello World, I am Sinenhlanhla";
-// // 
+// // let sentence = "Hello World, I am Sinenhlanhla";
+// // // 
 
-// for(let i=0; i<6; i++){
-// console.log(sentence[i]);
-// console.log(let of sentence);
-// };
+// // for(let i=0; i<6; i++){
+// // console.log(sentence[i]);
+// // console.log(let of sentence);
+// // };
+
+
+// // let listBook = ["book one", "book two", "book three"];
+// // for(let i=0; i<6; i++){
+// // console.log(listBook[i]);
+// // console.log(x of listBook);
+// // };
+
+// // let listsBook = ["book one", "book two", "book three"];
+// // let i=0;
+// // for(; i<6 ;){
+// //     i++;
+// // console.log(listsBook[i][0]);
+// // console.log(x of listsBook);
+// // };
+
+// const sentence = "Hello World, I'm Sinenhlanhla";
+
+// let texts = "";
+// for (let x of sentence) {
+//   texts += x;
+// //   console.log(x);
+// }
+
+// const bookList = ["book one", "book two", "book three"];
+
+
+// for (let x of cars) {
+//   i += x;
+// //   console.log(x);
+// }
 
 
 // let listBook = ["book one", "book two", "book three"];
+
+
+
+
+// let i =0;
+// while (i < 6){
 // for(let i=0; i<6; i++){
 // console.log(listBook[i]);
-// console.log(x of listBook);
+// // console.log(x of listBook);
 // };
+// }
 
-// let listsBook = ["book one", "book two", "book three"];
-// let i=0;
-// for(; i<6 ;){
-//     i++;
-// console.log(listsBook[i][0]);
-// console.log(x of listsBook);
-// };
-
-const sentence = "Hello World, I'm Sinenhlanhla";
-
-let texts = "";
-for (let x of sentence) {
-  texts += x;
-  console.log(x);
+let condition = false;
+do {
+  console.log("I am testing the do-while.");
+  condition = false;
 }
+while (condition);
 
-const bookList = ["BMW", "Volvo", "Mini"];
-
-let text = "";
-for (let x of cars) {
-  text += x;
-  console.log(k);
+let j = 10;
+do {
+  console.log("This is the second do-while.");
+  j++;
 }
+while (j<6);
