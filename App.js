648f5517ff855566c550fcdfe902ce855816017e
SineMag <snenhlanhlamagubane99@gmail.com>
2025-08-15 11:57:58 +0200

    Class session for loops.

---
+++ App.js
@@ -0,0 +1,38 @@
+// let sentence = "Hello World, I am Sinenhlanhla";
+// // 
+
+// for(let i=0; i<6; i++){
+// console.log(sentence[i]);
+// console.log(let of sentence);
+// };
+
+
+// let listBook = ["book one", "book two", "book three"];
+// for(let i=0; i<6; i++){
+// console.log(listBook[i]);
+// console.log(x of listBook);
+// };
+
+// let listsBook = ["book one", "book two", "book three"];
+// let i=0;
+// for(; i<6 ;){
+//     i++;
+// console.log(listsBook[i][0]);
+// console.log(x of listsBook);
+// };
+
+const sentence = "Hello World, I'm Sinenhlanhla";
+
+let texts = "";
+for (let x of sentence) {
+  texts += x;
+  console.log(x);
+}
+
+const bookList = ["BMW", "Volvo", "Mini"];
+
+let text = "";
+for (let x of cars) {
+  text += x;
+  console.log(k);
+}
---
+++ movies.js
@@ -0,0 +1,19 @@
+const movie = [{ //will have three objects ...
+
+// object one
+    title: "Naruto",
+    director: "Sinenhlanhla Magubane",
+    year: "2010",
+    genre: "Animation",
+    rating: 4,
+    actors: ["Joe", "Jaden", "Jill"]
+},
+// object two...
+{
+
+},
+//  object three..
+{
+
+
+}]
